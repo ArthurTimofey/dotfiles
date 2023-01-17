aeeca5428cfadab20eadce55e832e4e6f2ff4d69
ArthurTimofey <arthur.timofey@gmail.com>
2023-01-17 14:23:31 +0100

    fixes

--- .config/nvim/lua/plugins/config/auto-session.lua
+++ .config/nvim/lua/plugins/config/auto-session.lua
@@ -1,8 +1,8 @@
 require('auto-session').setup {
-	pre_save_cmds = { 'NvimTreeCollapse' },
+	pre_save_cmds = { 'NvimTreeToggle' },
 	pre_restore_cmds = { 'NvimTreeFindFileToggle' },
 	cwd_change_handling = {
 		restore_upcoming_session = true,
-		pre_restore_cmds = { 'NvimTreeOpen' }, -- having this or not, still same result
+		pre_restore_cmds = { 'NvimTreeFindFileToggle' }, -- having this or not, still same result
 	},
 }
